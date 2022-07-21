4de7c5fe6dde33511f6c6dabb0b9b69505fa6cf8
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-07-21 15:04:46 -0700

    remove contact

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import NormalLayout from '../components/NormalLayout'
-import Contact from '../components/contact'
 
 export default function Home ({ viewport }) {
   console.log(viewport);
