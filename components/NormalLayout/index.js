cb789708902817269dca469719d84e1df4182f43
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-07-21 10:49:15 -0700

    try and fix something

--- components/NormalLayout/index.js
+++ components/NormalLayout/index.js
@@ -4,7 +4,7 @@ import Hero from '../Hero'
 export default function NormalLayout ({ children, viewport }) {
     return (
         <>
-            <Nav collapsed={viewport.width < 800} />
+            <Nav collapsed={viewport?.width < 800} />
             <Hero viewport={viewport} />
             {children}
         </>
