408c00b1a404d38dc9c1b5268fbc12706f5a9c66
Yoda <yoda@yodacode.xyz>
2022-11-27 19:46:05 -0800

    Header

--- components/WallOfFame/index.js
+++ components/WallOfFame/index.js
@@ -3,6 +3,9 @@ import WallOfFameCopy from '../../copy/WallOfFame.mdx';
 export default function WallOfFame () {
     return (
         <div>
+            <center>
+                <h1>Hall of Fame</h1>
+            </center>
             <WallOfFameCopy />
         </div>
     )
