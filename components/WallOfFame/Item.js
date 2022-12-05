888093fad6c7a0fd736ba19111835b313e79334a
Yoda <yoda@yodacode.xyz>
2022-12-05 15:09:03 -0800

    Add Ernesto picture

--- components/WallOfFame/Item.js
+++ components/WallOfFame/Item.js
@@ -29,14 +29,14 @@ export default function Item ({ image, children }) {
     return (
         <>
             <center>
-                <div style={{ width: '100%', maxWidth: '900px' }}>
+                <div style={{ width: '100%', maxWidth: '1200px' }}>
                     <div style={viewport.styler.initial({
                         display: 'grid',
                         gridTemplateColumns: '1fr',
                         boxSizing: 'border-box'
                     }).md({
                         display: 'grid',
-                        gridTemplateColumns: '1fr min(100%, 400px)',
+                        gridTemplateColumns: '1fr min(100%, 500px)',
                         boxSizing: 'border-box'
                     }).styles}>
                         <div style={viewport.styler.initial({
@@ -60,7 +60,7 @@ export default function Item ({ image, children }) {
                             boxSizing: 'border-box',
                         }}>
                             <center style={{ boxSizing: 'border-box', width: '100%' }}>
-                                <img src={`/assets/wall-of-fame/${image}`} style={{ boxSizing: 'border-box', float: 'right', width: '100%', maxWidth: '350px', textAlign: 'center', border: '2px solid #1e2117', margin: '0px auto' }} />
+                                <img src={`/assets/wall-of-fame/${image}`} style={{ boxSizing: 'border-box', float: 'right', width: '100%', maxWidth: '600px', textAlign: 'center', border: '2px solid #1e2117', margin: '0px auto' }} />
                             </center>
                         </div>
                     </div>
